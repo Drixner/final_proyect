32394d2031facdb0fe1c9e1c4a5ef9accc161e03
Drixner <drixnerrafael1@gmail.com>
2023-02-04 04:05:22 -0500

    fix: correct docstring

--- Backend/FastAPI/users.py
+++ Backend/FastAPI/users.py
@@ -4,60 +4,83 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
-# inicia el server: uvicorn users:app --reload
-# entidad user
+# Inicia el server: uvicorn users:app --reload
 
+# Entidad user
 class User(BaseModel):
-    " la clase User usada para definir todas las varibles y estas a la vez estan formateadas"
+    " la clase User usada para definir todas las varibles y a su vez, formateadas"
     id: int
     name: str
     surname: str
     url: str
     age: int
 
-
+# Lista de usuarios
 users_list = [User(id=1, name="Drixner", surname="Condor",
             url="https://drixner.github.io/myportfolio/", age=30),
             User(id=2, name="Yngrid", surname="Barrera", url="google.com", age=24),
             User(id=3, name="Kattie", surname="Condor", url="maps.com", age=24)]
 
-#users_list = [User(name="Brais", surname="Moure", url="https://moure.dev", age=35),
-#              User(name="Moure", surname="Dev",
-#                   url="https://mouredev.com", age=35),
-#              User(name="Brais", surname="Dahlberg", url="https://haakon.com", age=33)]
 
 @app.get("/usersjson")
-async def usersjson():
-    "--Esto es una base de datos provicional--"
+async def users_json():
+    """
+    Devuelve la lista de usuarios en formato JSON
+    """
 
-    return [{"name":"Drixner", "surname": "Condor",
-            "url": "https://drixner.github.io/myportfolio/", "age": 30},
-            {"name":"Yngrid", "surname": "Barrera",
-            "url": "https://drixner.github.io/myportfolio/", "age": 23},
-            {"name":"Kattie", "surname": "Condor",
-            "url": "https://drixner.github.io/myportfolio/", "age": 26}]
+    return [
+        {
+            "name": "Drixner",
+            "surname": "Condor",
+            "url": "https://drixner.github.io/myportfolio/",
+            "age": 30
+        },
+        {
+            "name": "Yngrid",
+            "surname": "Barrera",
+            "url": "https://drixner.github.io/myportfolio/",
+            "age": 23
+        },
+        {
+            "name": "Kattie",
+            "surname": "Condor",
+            "url": "https://drixner.github.io/myportfolio/",
+            "age": 26
+        }
+    ]
 
 
 @app.get("/users")
 async def users():
-    "--el portal para consultar lista de usuarios--"
+    """
+    Devuelve la lista de usuarios
+    """
     return users_list
 
 #path
 @app.get("/user/{id}")
 async def user(id: int):
+    """
+    Devuelve un usuario por su id
+    """
     return search_user(id)
 
 
 #query
 @app.get("/user/")
 async def user(id: int):
+    """
+    Devuelve un usuario por su id
+    """
     return search_user(id)
 
 
 #   METODO POST insertar valores
 @app.post("/user/", status_code=201)
-async def user(user: User):
+async def create_user(user: User):
+    """
+    Crea un nuevo usuario
+    """
     if isinstance(search_user(user.id)) == User:
         HTTPException(status_code=204, detail="El usuario ya existe")
         return {"Error":"El usuario ya existe"}
@@ -68,15 +91,16 @@ async def user(user: User):
 
 #   METODO PUT
 @app.put("/user/")
-async def user(user:User):
+async def update_user(user: User):
+    """
+    Actualiza un usuario existente
+    """
 
     found = False
-
     for index, saved_user in enumerate(users_list):
         if saved_user.id == user.id:
             users_list[index] = user
             found = True
-
     if not found:
         return {'error': 'No se ha actualizado el usuario'}
     return user
@@ -84,15 +108,15 @@ async def user(user:User):
 
 # METODO DELETE
 @app.delete("/user/{id}")
-async def user(id: int):
-
+async def delete_user(id: int):
+    """
+    Elimina un usuario existente
+    """
     found = False
-
     for index, saved_user in enumerate(users_list):
         if saved_user.id == id:
             del users_list[index]
             found = True
-
     if not found:
         return {'error': 'No se ha eliminado el usuario'}
 # <-- Check
