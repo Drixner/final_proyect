1960492869fc5ed5db0bb953fb2300f0b642e70f
Drixner <drixnerrafael1@gmail.com>
2023-02-06 01:05:36 -0500

    add: modify routing

--- Backend/FastAPI/main.py
+++ Backend/FastAPI/main.py
@@ -1,12 +1,25 @@
+"""
+inicializa la aplicacion
+"""
 from fastapi import FastAPI
+from routers import products
 
 app = FastAPI()
 
+# routers
+app.include_router(products.router)
+
 @app.get("/")
 async def root():
+    """
+    es la ruta principal
+    """
     return "Hola Drixner!"
 
 
 @app.get("/url")
 async def url():
+    """
+    Devuelve la url de mi portafolio
+    """
     return {"url":"https://drixner.github.io/myportfolio/"}
