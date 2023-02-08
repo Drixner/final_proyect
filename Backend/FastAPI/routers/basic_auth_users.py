eaa0a27bd612fd669166f9d50a7a2a6fc0f21243
Drixner <drixnerrafael1@gmail.com>
2023-02-08 17:51:25 -0500

    add: new line for autentication

--- Backend/FastAPI/routers/basic_auth_users.py
+++ Backend/FastAPI/routers/basic_auth_users.py
@@ -1,16 +1,31 @@
-from fastapi import FastAPI
+"""
+inicializacion de la autenticacion basica
+"""
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from fastapi import FastAPI,Depends, HTTPException
 from pydantic import BaseModel
 
 
 
 app = FastAPI()
 
+oauth2 = OAuth2PasswordBearer(tokenUrl="login")
+
 
 class User(BaseModel):
+    """
+    Clase para el usuario
+    """
     username: str
     full_name: str
     email: str
     disabled: bool
+
+class UserDB(User):
+    """
+    Clase para la base de datos de usuarios, que vela por la contrasena
+    """
+    password: str
 
 
 users_db = {
@@ -20,5 +35,38 @@ users_db = {
         "email" : "drixner@gmail.com",
         "disabled": False,
         "password": "123drix",
-        }
+    },
+
+    "Yngrid": {
+        "username" : "yngrid",
+        "full_name" : "Yngrid Barrera",
+        "email" : "titi@gmail.com",
+        "disabled": True,
+        "password": "123titi",
     }
+}
+
+def search_user(username: str):
+    """
+    para la busqueda del usuario
+    """
+    if username in users_db:
+        return UserDB(**users_db[username])
+
+
+@app.post("/login")
+async def login(form: OAuth2PasswordRequestForm = Depends()):
+    """
+    funcion para autenticar al usuario
+    """
+    user_db = users_db.get(form.username)
+    if not user_db:
+        raise HTTPException(
+                status_code=400, detail="El usuario no es correcto")
+
+    user = search_user(form.username)
+    if not form.password == user.password:
+        raise HTTPException(
+                status_code=400, detail="La contraseña no es correcta")
+
+    return {"access_token": user.username, "token_type": "bearer"}
