ef804a38910e6bef996ecf2f1cbedeb9a768c96c
Drixner <drixnerrafael1@gmail.com>
2023-02-08 11:11:22 -0500

    add: new changes in formatter

--- Backend/FastAPI/routers/products.py
+++ Backend/FastAPI/routers/products.py
@@ -3,11 +3,27 @@ modudule of producto
 """
 from fastapi import APIRouter
 
-router = APIRouter()
 
-@router.get("/products")
+
+router = APIRouter(prefix="/products",
+                tags=["products"],# se utiliza para la documentacion
+                responses={404: {"messages": "No encontrado"}})
+
+products_list = [
+        "producto0", "producto2", "producto3","producto4", "producto5", "producto6"
+        ]
+
+@router.get("")
 async def products():
     """
     Devuelve la lista de productos
     """
-    return ["producto1", "producto2", "producto3","producto4", "producto5", "producto6"]
+    return products_list
+
+
+@router.get("/{id}")
+async def products_id(id: int):
+    """
+    Devuelve la lista de productos
+    """
+    return products_list[id]
