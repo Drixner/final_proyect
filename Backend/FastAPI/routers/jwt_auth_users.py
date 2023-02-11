74465594db5bd94b14dcb97e075a37454eaa5d08
Drixner <drixnerrafael1@gmail.com>
2023-02-11 01:04:52 -0500

    add: update token

--- Backend/FastAPI/routers/jwt_auth_users.py
+++ Backend/FastAPI/routers/jwt_auth_users.py
@@ -11,6 +11,7 @@ from datetime import datetime, timedelta
 
 ALGORITHM = "HS256"  #algoritmo de encriptacion
 ACCESS_TOKEN_EXPIRE_MINUTES = 1  #tiempo de expiracion del token
+SECRET="13d6c7dc98ffcb88114e8b72401b6b03dd96a151346b1a71c90a24d97e603095"  #clave secreta
 
 app = FastAPI()  #inicializacion de la aplicacion
 
@@ -60,6 +61,26 @@ def search_user_db(username: str): #para la busqueda del usuarios
     if username in users_db:
         return UserDB(**users_db[username])
 
+async def auth_user(token: str):
+
+
+#funcion 
+async def current_user(token: str = Depends(oauth2)):
+    """
+    funcion para el usuario actual
+    """
+    user = search_user(token)
+    if not user:
+        raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Credenciales de autorizacion invalidas",
+                headers={"WWW-authenticate":"bearer"})
+
+    if user.disabled:
+        raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="Usuario deshabilitado")
+    return user
 
 
 
@@ -84,4 +105,12 @@ async def login(form: OAuth2PasswordRequestForm = Depends()):
     access_token = {"sub": user.username,
                     "exp": datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)}
 
-    return {"access_token": access_token, "token_type": "bearer"}
+    return {"access_token": jwt.encode(access_token, SECRET,algorithm=ALGORITHM), "token_type": "bearer"}
+
+
+@app.get("/users/me")
+async def read_users_me(user: User = Depends(current_user)):
+    """
+    funcion para leer el usuario
+    """
+    return user
